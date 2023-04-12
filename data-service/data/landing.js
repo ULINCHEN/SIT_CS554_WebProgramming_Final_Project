37b6debcfdb61091b0f3bda6c9dabea1b2f17e11
Sexy Hamilton <wanghanyu329@gmail.com>
2023-04-11 22:13:23 -0400

    createPets, UpdatePets and their routes

--- data-service/data/landing.js
+++ data-service/data/landing.js
@@ -1,63 +1,182 @@
-import { pets } from '../config/mongoCollections.js';
-import { ObjectId } from 'mongodb';
-import bcrypt from 'bcryptjs';
-import validation from '../validation/pets.js';
-import petsData from './pets.js';
+import { pets } from "../config/mongoCollections.js";
+import { ObjectId } from "mongodb";
+import bcrypt from "bcryptjs";
+import validation from "../validation/pets.js";
+import petsData from "./pets.js";
 const saltRounds = 10;
 
-const createPet = async(username, email, password) => {
-    username = validation.checkUsername(username);
-    email = validation.checkEmail(email);
-    password = validation.checkPassword(password);
+const createPet = async (
+  username,
+  email,
+  password,
+  nickname,
+  breed,
+  age,
+  sex,
+  DOB,
+  hobby,
+  personality,
+  preference
+) => {
+  username = validation.checkUsername(username);
+  email = validation.checkEmail(email);
+  password = validation.checkPassword(password);
+  nickname = validation.checkNickname(nickname);
+  breed = validation.checkBreed(breed);
+  age = validation.checkAge(age);
+  sex = validation.checkSex(sex);
+  DOB = validation.checkDOB(DOB);
+  hobby = validation.checkHobbies(hobby);
+  personality = validation.checkPersonality(personality);
+  preference = validation.checkPreferences(preference);
 
-    const petsCol = await pets();
+  const petsCol = await pets();
 
-    const petExists = await petsCol.findOne({username: username});
-    if (petExists) {
-        throw 'Wrong user name and password';
-    }
+  const petExists = await petsCol.findOne({ username: username });
+  if (petExists) {
+    throw "Wrong user name and password";
+  }
 
-    const hashed_pw = await bcrypt.hash(password, saltRounds);
+  const hashed_pw = await bcrypt.hash(password, saltRounds);
 
-    let newPet = {
-        username: username,
-        email: email,
-        hashed_password: hashed_pw
-    }
+  let newPet = {
+    username: username,
+    email: email,
+    hashed_password: hashed_pw,
+    nickname: nickname,
+    breed: breed,
+    age: age,
+    sex: sex,
+    DOB: DOB,
+    hobby: hobby,
+    personality: personality,
+    preference: preference,
+  };
 
-    const insertInfo = await petsCol.insertOne(newPet);
+  const insertInfo = await petsCol.insertOne(newPet);
 
-    if (!insertInfo.acknowledged || !insertInfo.insertedId) {
-        throw 'Could not create a user';
-    }
+  if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+    throw "Could not create a user";
+  }
 
-    const newPet_id = insertInfo.insertedId.toString();
+  const newPet_id = insertInfo.insertedId.toString();
 
-    return await petsData.getPetById(newPet_id);
-}
+  return await petsData.getPetById(newPet_id);
+};
 
-const checkUser = async(username, password) => {
-    username = validation.checkUsername(username);
-    password = validation.checkPassword(password);
+const checkUser = async (username, password) => {
+  username = validation.checkUsername(username);
+  password = validation.checkPassword(password);
 
-    const petsCol = await pets();
+  const petsCol = await pets();
 
-    //query the db for username
-    const oldUser = await petsCol.findOne({username: username});
-    if (!oldUser) {
-        throw 'Either the username or password is invalid!';
-    }
+  //query the db for username
+  const oldUser = await petsCol.findOne({ username: username });
+  if (!oldUser) {
+    throw "Either the username or password is invalid!";
+  }
 
-    //compare the password
-    const oldPw = oldUser.hashed_password;
-    const compareResult = await bcrypt.compare(password, oldPw);
-    if (!compareResult) {
-        throw 'Either the username or password is invalid!';
-    }
-    return await petsData.getPetById(oldUser._id.toString());
-}
+  //compare the password
+  const oldPw = oldUser.hashed_password;
+  const compareResult = await bcrypt.compare(password, oldPw);
+  if (!compareResult) {
+    throw "Either the username or password is invalid!";
+  }
+  return await petsData.getPetById(oldUser._id.toString());
+};
+
+const getPetsById = async (id) => {
+  const petsCol = await pets();
+  let pet = await petsCol.findOne({ _id: ObjectId(id) });
+  if (pet === null) throw "No recipe with that id";
+  return pet;
+};
+
+const updatePets = async (id, updateFields) => {
+  const oldPet = await this.getPetsById(id);
+  let newPet = {};
+  let numOfFieldToUpdate = 0;
+  if (updateFields.email != undefined && updateFields.email !== oldPet.email) {
+    newPet.email = validation.checkEmail(updateFields.email);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.email = oldPet.email;
+  }
+  if (
+    updateFields.nickname != undefined &&
+    updateFields.nickname !== oldPet.nickname
+  ) {
+    newPet.nickname = validation.checkNickname(updateFields.nickname);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.nickname = oldPet.nickname;
+  }
+  if (updateFields.breed != undefined && updateFields.breed !== oldPet.breed) {
+    newPet.breed = validation.checkBreed(updateFields.breed);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.breed = oldPet.breed;
+  }
+  if (updateFields.sex != undefined && updateFields.sex !== oldPet.sex) {
+    newPet.sex = validation.checkSex(updateFields.sex);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.sex = oldPet.sex;
+  }
+  if (updateFields.age != undefined && updateFields.age !== oldPet.age) {
+    newPet.age = validation.checkAge(updateFields.age);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.age = oldPet.age;
+  }
+  if (updateFields.DOB != undefined && updateFields.DOB !== oldPet.DOB) {
+    newPet.DOB = validation.checkDOB(updateFields.DOB);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.DOB = oldPet.DOB;
+  }
+  if (updateFields.hobby != undefined && updateFields.hobby !== oldPet.hobby) {
+    newPet.hobby = validation.checkHobbies(updateFields.hobby);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.hobby = oldPet.hobby;
+  }
+  if (
+    updateFields.personality != undefined &&
+    updateFields.personality !== oldPet.personality
+  ) {
+    newPet.personality = validation.checkPersonality(updateFields.personality);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.personality = oldPet.personality;
+  }
+  if (
+    updateFields.preference != undefined &&
+    updateFields.preference !== oldPet.preference
+  ) {
+    newPet.preference = validation.checkPreferences(updateFields.preference);
+    numOfFieldToUpdate++;
+  } else {
+    updateFields.preference = oldPet.preference;
+  }
+
+  if (numOfFieldToUpdate == 0) {
+    throw "there is nothing need to update";
+  }
+  const petsCol = await pets();
+  const updateInfo = await petsCol.updateOne(
+    { _id: ObjectId(id) },
+    { $set: newPet }
+  );
+  if (!updateInfo.matchedCount && !updateInfo.modifiedCount)
+    throw "Update failed";
+
+  return await this.getPetsById(id);
+};
 
 export default {
-    createPet,
-    checkUser
-}
+  createPet,
+  checkUser,
+  updatePets,
+  getPetsById,
+};
--- data-service/routes/landing.js
+++ data-service/routes/landing.js
@@ -1,67 +1,132 @@
 import express from "express";
 const router = express.Router();
-import data from '../data/index.js';
-import validation from '../validation/pets.js'
+import data from "../data/index.js";
+import validation from "../validation/pets.js";
 const landingData = data.landing;
 
+router.route("/signup").post(async (req, res) => {
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  let nickname = req.body.nickname;
+  let breed = req.body.breed;
+  let age = req.body.age;
+  let sex = req.body.sex;
+  let DOB = req.body.DOB;
+  let hobby = req.body.hobby;
+  let personality = req.body.personality;
+  let preference = req.body.preference;
 
-router
-    .route('/signup')
-    .post( async (req, res) => {
-        let username = req.body.username;
-        let email = req.body.email;
-        let password = req.body.password;
+  try {
+    username = validation.checkUsername(username);
+    email = validation.checkEmail(email);
+    password = validation.checkPassword(password);
+    nickname = validation.checkNickname(nickname);
+    breed = validation.checkBreed(breed);
+    age = validation.checkAge(age);
+    sex = validation.checkSex(sex);
+    DOB = validation.checkDOB(DOB);
+    hobby = validation.checkHobbies(hobby);
+    personality = validation.checkPersonality(personality);
+    preference = validation.checkPreferences(preference);
+  } catch (e) {
+    return res.status(400).json({ Error: e });
+  }
 
-        try {
-            username = validation.checkUsername(username);
-            email = validation.checkEmail(email);
-            password = validation.checkPassword(password);
-        }catch (e) {
-            return res.status(400).json({Error: e});
-        }
+  try {
+    const newPet = await landingData.createPet(
+      username,
+      email,
+      password,
+      nickname,
+      breed,
+      age,
+      sex,
+      DOB,
+      hobby,
+      personality,
+      preference
+    );
 
-        try {
-            const newPet = await landingData.createPet(username, email, password);
+    res.status(200).json(newPet);
+  } catch (e) {
+    return res.status(500).json({ Error: e });
+  }
+});
 
-            res.status(200).json(newPet);
-        }catch (e) {
-            return res.status(500).json({Error: e});
-        }
-    })
+router.route("/login").post(async (req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
 
-router
-    .route('/login')
-    .post(async (req, res) => {
-        let username = req.body.username;
-        let password = req.body.password;
+  try {
+    username = validation.checkUsername(username);
+    password = validation.checkPassword(password);
+  } catch (e) {
+    return res.status(400).json({ Error: e });
+  }
 
-        try{
-            username = validation.checkUsername(username);
-            password = validation.checkPassword(password);
-        }catch (e) {
-            return res.status(400).json({Error: e});
-        }
+  try {
+    const pet = await landingData.checkUser(username, password);
 
-        try{
-            const pet = await landingData.checkUser(username, password);
+    req.session.pet = {
+      petId: pet._id.toString(),
+    };
 
-            req.session.pet = {
-                petId: pet._id.toString()
-            }
+    res.status(200).json(pet);
+  } catch (e) {
+    return res.status(400).json({ Error: e });
+  }
+});
 
-            res.status(200).json(pet);
+//暂定update的修改按钮在profile里
+router.route("/profile/:id").patch(async (req, res) => {
+  const petId = req.params.id;
+  let updatePetResult = undefined;
+  let updateFields = {};
+  if (req.body.email != undefined) {
+    updateFields.email = req.body.email;
+  }
+  if (req.body.nickname != undefined) {
+    updateFields.nickname = req.body.nickname;
+  }
+  if (req.body.breed != undefined) {
+    updateFields.breed = req.body.breed;
+  }
+  if (req.body.age != undefined) {
+    updateFields.age = req.body.age;
+  }
+  if (req.body.sex != undefined) {
+    updateFields.sex = req.body.sex;
+  }
+  if (req.body.DOB != undefined) {
+    updateFields.DOB = req.body.DOB;
+  }
+  if (req.body.hobby != undefined) {
+    updateFields.hobby = req.body.hobby;
+  }
+  if (req.body.personality != undefined) {
+    updateFields.personality = req.body.personality;
+  }
+  if (req.body.preference != undefined) {
+    updateFields.preference = req.body.preference;
+  }
+  try {
+    updatePetResult = await landingData.updatePets(petId, updateFields);
+  } catch (e) {
+    res.status(400).json({ error: e });
+    return;
+  }
+  if (updatePetResult != undefined) {
+    res.status(200).json(updatePetResult);
+  } else {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+  return;
+});
 
-        }catch (e) {
-            return res.status(400).json({Error: e});
-        }
-    })
-
-
-router
-    .route('/logout')
-    .post(async (req, res) => {
-        res.session.destroy();
-        res.status(200).json({Message: 'You have been logged out'});
-    })
+router.route("/logout").post(async (req, res) => {
+  res.session.destroy();
+  res.status(200).json({ Message: "You have been logged out" });
+});
 
 export default router;
