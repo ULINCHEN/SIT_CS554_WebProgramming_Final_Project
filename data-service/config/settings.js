dae0c351bb5fb011ccc170856eb249345e9b44a1
Ziheng Zhu <zihzhu1@gmail.com>
2023-04-29 17:57:59 -0400

    sorry for change the mongoConfig

--- data-service/config/settings.js
+++ data-service/config/settings.js
@@ -1,9 +1,5 @@
-// export const mongoConfig = {
-//     serverUrl: "mongodb+srv://finalproject:5C1jpTzjjWSO7jsC@cluster0.pbfzfk2.mongodb.net/?retryWrites=true&w=majority",
-//     database: 'CS554Final_Project'
-//   };
-
 export const mongoConfig = {
-  serverUrl: 'mongodb://localhost:27017/',
-  database: 'Ziheng-Zhu-CS554Final_Project'
-};
+    serverUrl: "mongodb+srv://finalproject:5C1jpTzjjWSO7jsC@cluster0.pbfzfk2.mongodb.net/?retryWrites=true&w=majority",
+    database: 'CS554Final_Project'
+  };
+
